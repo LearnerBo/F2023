949fbd77322eff44a8d0b232e8e3a2a05fc25ef6
LearnerBo <1950380700@qq.com>
2023-09-17 22:14:37 +0800

    触控屏界面

---
+++ F2023(触控屏工程)/output/DefineID.h
@@ -0,0 +1,30 @@
+//创建画面Screen0的命令索引ID
+#define  _SCREEN_SCREEN0                                                       0
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY1                                       1
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY2                                       2
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY3                                       3
+
+#define  _BTN_SCREEN0_BUTTON1                                                  4
+
+//全局图片按下显示.bmp
+#define  _IMG_按下显示_BMP                                                         1
+
+#define  _BTN_SCREEN0_BUTTON2                                                  5
+
+#define  _BTN_SCREEN0_BUTTON3                                                  6
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY4                                       7
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY5                                       8
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY6                                       9
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY7                                      10
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY8                                      11
+
+#define  _TXT_DIS__SCREEN0_TEXT_DISPLAY9                                      12
+
